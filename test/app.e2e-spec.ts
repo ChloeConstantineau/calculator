7032e1f53b9308c237ee3375f1c6e9631f52983c
ChloeConstantineau <chloe_constantineau@hotmail.com>
2023-01-04 14:43:56 -0500

    fix e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { APP_PIPE } from '@nestjs/core';
+import { CalculusResponse } from 'src/calculus/calculus.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -27,9 +28,13 @@ describe('AppController (e2e)', () => {
   });
 
   it('/calculus?query=... (GET)', () => {
+    const result: CalculusResponse = {
+      error: false,
+      result: 982 + 23 / (3 * 3) - 23 * (2 * 3),
+    };
     return request(app.getHttpServer())
-      .get('/calculus?query=OTgyKygyMy8oMyogMykpLTIzKigyKjMgKSA=')
+      .get('/calculus?query=OTgyKygyMy8oMyozKSktMjMqKDIqMyk=')
       .expect(200)
-      .expect('Query is: 982+(23/(3*3))-23*(2*3)');
+      .expect(result);
   });
 });
