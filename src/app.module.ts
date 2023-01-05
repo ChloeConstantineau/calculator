8db5da15807dc768bc353ae4125b305d3a9649f8
ChloeConstantineau <chloe_constantineau@hotmail.com>
2023-01-05 16:46:24 -0500

    Removing http health check - opting to use TCP instead

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CalculusModule } from './calculus/calculus.module';
-import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [HealthModule, CalculusModule, ConfigModule.forRoot()],
+  imports: [CalculusModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
